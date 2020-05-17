04b942c08aeaeffa4e4cf30c1fce9d229c2f05ef
AndreiIovescu <61495316+AndreiIovescu@users.noreply.github.com>
2020-05-18 00:13:43 +0300

    Update Index.cshtml.cs

--- AutomatedDispatcher/AutomatedDispatcher/Pages/Index.cshtml.cs
+++ AutomatedDispatcher/AutomatedDispatcher/Pages/Index.cshtml.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 using AutomatedDispatcher.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 using Microsoft.Extensions.Logging;
 
